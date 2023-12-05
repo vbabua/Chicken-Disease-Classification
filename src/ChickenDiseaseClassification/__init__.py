ec0be3716d346bd06d797999dd65b0b13a024629
yoonpaix <babuvadakemu@gmail.com>
2023-12-05 22:03:15 +0000

    Adding logging file with Comments

--- src/ChickenDiseaseClassification/__init__.py
+++ src/ChickenDiseaseClassification/__init__.py
@@ -2,21 +2,29 @@ import os
 import sys
 import logging
 
+# Logging format string
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
+# Directory and file path for logging
 log_dir = "logs"
-log_filepath = os.path.join(log_dir,"running_logs.log")
-os.makedirs(log_dir, exist_ok=True)
+log_filepath = os.path.join(log_dir, "running_logs.log")
+os.makedirs(log_dir, exist_ok=True)  
 
-
+# Basic configuration for logging
 logging.basicConfig(
-    level= logging.INFO,
-    format= logging_str,
+    level=logging.INFO, 
 
+    # Use the defined logging format                
+    format=logging_str,                 
     handlers=[
-        logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout)
+        
+        # File handler to write logs to a file
+        logging.FileHandler(log_filepath), 
+
+        # Stream handler to output logs to the console 
+        logging.StreamHandler(sys.stdout)   
     ]
 )
 
+# Creating a logger instance
 logger = logging.getLogger("cnnClassifierLogger")
