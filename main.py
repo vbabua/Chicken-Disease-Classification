0b5fb391964c17d4c3ba8524ce828b56a70d1fd1
yoonpaix <babuvadakemu@gmail.com>
2023-12-06 20:34:06 +0000

    Completed Data Ingestion

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from ChickenDiseaseClassification import logger
+from ChickenDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Testing Custom log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/ChickenDiseaseClassification/utils/common.py
+++ src/ChickenDiseaseClassification/utils/common.py
@@ -2,7 +2,7 @@
 import os
 from pathlib import Path
 import yaml
-from box import ConfigBox, BoxValueError
+from box import ConfigBox
 from ChickenDiseaseClassification import logger  
 import joblib
 from ensure import ensure_annotations
